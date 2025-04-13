134b7d29bebbfb7e833d51afbdd3e0bf59994c20
Romain <roroblyat@gmail.com>
2025-04-13 07:02:04 +0200

    (i18n) Config fichier sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,7 +8,8 @@ redis_host = ENV.fetch('REDIS_HOST', 'localhost')
 redis_url = "redis://#{redis_host}:6379"
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+    config.redis = { url: 'redis://localhost:6379/0' } # Forcer localhost
+  # config.redis = { url: redis_url }
 
   # client_middleware is also configured in configure_server block
   # because jobs running in the Sidekiq server can themselves push
@@ -38,7 +39,8 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: 'redis://localhost:6379/0' } # Forcer localhost
+  # config.redis = { url: redis_url }
 
   config.client_middleware do |chain|
     chain.add Sidekiq::Middleware::I18n::Client
