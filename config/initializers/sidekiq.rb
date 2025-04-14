b8f7a113e6af944c010fddeb8cff746fb12b4cdb
Romain <roroblyat@gmail.com>
2025-04-14 23:21:11 +0200

    (i18n) Renom des variables database

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -39,7 +39,7 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' } # Forcer localhost
+    config.redis = { url: 'redis://localhost:6379/0' } # Forcer localhost
   # config.redis = { url: redis_url }
 
   config.client_middleware do |chain|
