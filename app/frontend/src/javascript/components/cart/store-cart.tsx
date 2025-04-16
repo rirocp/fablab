373352cacd2a85d448dd81f76a6724109be3b49f
Romain <roroblyat@gmail.com>
2025-04-16 13:17:10 +0200

    (i18n) Modification code promo store-cart.tsx 3

--- app/frontend/src/javascript/components/cart/store-cart.tsx
+++ app/frontend/src/javascript/components/cart/store-cart.tsx
@@ -191,16 +191,18 @@ const StoreCart: React.FC<StoreCartProps> = ({ onSuccess, onError, currentUser, 
             <CouponInput user={cart.user as User} amount={cart.total} onChange={applyCoupon} />
           </div>
         }*/}
-      <div className="store-cart-coupon">
-        <h3>Déroulement du prêt avec nous</h3>
-        <p><strong>Confirmation du prêt</strong> : Une fois votre commande passée et votre prêt validé lors du rendez-vous, vous recevrez un email de confirmation pour vous assurer que tout est en ordre.</p>
-        <p><strong>Rappel avant la fin du délai</strong> : Une semaine avant l’écoulement du délai de prêt, nous vous enverrons un email pour vous rappeler de retourner l’article.</p>
-        <p><strong>Notification en cas d’annulation</strong> : Si votre prêt est annulé pour une raison quelconque, vous serez immédiatement informé par email.</p>
-      </div>
+
+        {cart && !cartIsEmpty() &&
+          <div className="store-cart-info">
+            <h3>Déroulement du prêt avec nous</h3>
+            <p><strong>Confirmation du prêt</strong> : Une fois votre commande passée et votre prêt validé lors du rendez-vous, vous recevrez un email de confirmation pour vous assurer que tout est en ordre.</p>
+            <p><strong>Rappel avant la fin du délai</strong> : Une semaine avant l’écoulement du délai de prêt, nous vous enverrons un email pour vous rappeler de retourner l’article.</p>
+            <p><strong>Notification en cas d’annulation</strong> : Si votre prêt est annulé pour une raison quelconque, vous serez immédiatement informé par email.</p>
+          </div>
+          }
 
       </div>
       
-
       <aside>
         {cart && !cartIsEmpty() && isPrivileged() &&
           <div> <MemberSelect onSelected={handleChangeMember} defaultUser={cart.user as User} hasError={noMemberError} /></div>
