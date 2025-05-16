a448ddbf0c7874bb8af6e6f4da176ec09d4271b2
rirocp <schneider.romain3@gmail.com>
2025-05-16 14:38:14 +0200

    version finale.Melissa - correction de nBadge

--- app/frontend/src/javascript/lib/validation.ts
+++ app/frontend/src/javascript/lib/validation.ts
@@ -2,5 +2,5 @@
 export default class ValidationLib {
   static urlRegex = /^(https?:\/\/)(([^.]+)\.)+(.{2,30})(\/.*)*\/?$/;
   static endpointRegex = /^\/?([-._~:?#[\]@!$&'()*+,;=%\w]+\/?)*$/;
-  static phoneRegex = /^((00|\+)\d{2,3})?[\d -]{4,14}$/;
+  static phoneRegex = /^\d+$/;
 }
